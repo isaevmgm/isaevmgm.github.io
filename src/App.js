3ceb0f07c8df0d334cd18819299a855c49a355f7
Magomed <eisenbergg13@mail.ru>
2020-11-26 23:33:38 +0300

    reduxnew

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => {
+                return response.json()
+            })
+            .then((json) => {
+                setData(json);
+                setLoading(false);
+            })
+    }, []);
+
+
+
+    return (
+        <div className='main'>
+            <div>
+                <div className='person-data'>
+                    Данные о пользователях:
+                </div>
+            </div>
+            <div className='block'>
+                {loading ? (
+                    <div>
+                        идет загрузка
+                    </div>
+                ) : data.map(item => {
+                    return(
+                        <div>
+                            <div className='elem'>
+                                <span className='element'>Имя:</span> {item.name}
+                            </div>
+                            <div className='elem'>
+                                <span className='element'>Email:</span> {item.email}
+                            </div>
+                            <div className='elem'>
+                                <span className='element'>Номер телефона:</span> {item.phone}
+                            </div>
+                            <div className='elemc'>
+                                <span className='elementc'>Город:</span> {item.address.city}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    )
 }
 
 export default App;
